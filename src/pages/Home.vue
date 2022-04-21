569a7ca9f519852a22f594a2efcf7e79c59386fb
Meruem117 <1572677319@qq.com>
2022-04-21 23:14:12 +0800

    add

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,33 @@
 <template>
-  <h1>Home</h1>
+  <div class="container">
+    <h5>Image</h5>
+    <van-uploader :after-read="afterRead" />
+  </div>
 </template>
 
 <script>
+import { Uploader } from 'vant'
+
 export default {
-  name: 'HomePage'
+  name: 'HomePage',
+  components: {
+    [Uploader.name]: Uploader
+  },
+  data() {
+    return {}
+  },
+  methods: {
+    afterRead(file) {
+      console.log(file)
+    }
+  }
 }
 </script>
+
+<style scoped>
+.container {
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+}
+</style>
